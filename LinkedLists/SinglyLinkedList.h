ea320687bf09a33427d366b39e278761fb33ba70
japumba <jdelamorav@hotmail.com>
2014-01-31 14:24:57 -0600

    Singly Linked list basic functions added: createNode, addNode and printList.

--- LinkedLists/SinglyLinkedList.h
+++ LinkedLists/SinglyLinkedList.h
@@ -7,13 +7,18 @@ typedef struct NODE
 {
 	int value;
 	struct NODE *next;
-}Node
+}Node;
 
 typedef struct SINGLY_LIST
 {
 	struct NODE *head;
 	int count;
-}SinglyList
+}SinglyList;
+
+Node *createNode(int value);
+SinglyList *createSinglyList();
+void addNode(SinglyList *list, int value);
+void printList(SinglyList *list);
 
 SinglyList *createSinglyList()
 {
@@ -22,6 +27,51 @@ SinglyList *createSinglyList()
 	newList->count = 0;
 	newList->head = NULL;
 	return newList;
+}
+
+Node *createNode(int value)
+{
+	Node *newNode = (Node *)malloc(sizeof(Node));
+	newNode->value = value;
+	newNode->next = NULL;
+	printf("New node created with value: %i.",value);
+	return newNode;
+}
+
+void addNode(SinglyList *list, int value)
+{
+	if(list == NULL)
+		return;
+
+	Node *newNode;
+	newNode = createNode(value);
+
+	if(list->count == 0)
+	{
+		list->head = newNode;
+		list->count++;
+		printf("List is empty, adding new node.\n");
+		return;
+	}
+	
+	Node *temp = list->head;
+	while(temp->next != NULL)
+		temp = temp->next;
+	temp->next = newNode;
+	list->count++;
+	printf("Node inserted at the end of list.\n");
+}
+
+void printList(SinglyList *list)
+{
+	if(list == NULL)
+		return;
+	Node *temp = list->head;
+	while(temp != NULL)
+	{
+		printf("[%i]\n",temp->value);
+		temp = temp->next;
+	}
 }
 
 #endif
--- LinkedLists/main.c
+++ LinkedLists/main.c
@@ -6,6 +6,10 @@ int main()
 	SinglyList *list;
 
 	list = createSinglyList();
-	printf("list's address is: %p",list);
+	printf("list's address is: %p\n",list);
+	addNode(list,1);
+	addNode(list,2);
+	printf("list's count is: %i\n",list->count);
+	printList(list);
 	return 0;
 }
