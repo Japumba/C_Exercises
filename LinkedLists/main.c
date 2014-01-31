09388266b6e5d6c49bfe1b8dad086d857325bfbc
japumba <jdelamorav@hotmail.com>
2014-01-31 16:14:40 -0600

    Added deleteList and emptyList to singlyLinkedList.h and updated main.c to use them in the test.

--- LinkedLists/main.c
+++ LinkedLists/main.c
@@ -3,13 +3,17 @@
 
 int main()
 {
+	
 	SinglyList *list;
-
+	
 	list = createSinglyList();
 	printf("list's address is: %p\n",list);
 	addNode(list,1);
 	addNode(list,2);
 	printf("list's count is: %i\n",list->count);
 	printList(list);
+	
+
+	deleteList(list);
 	return 0;
 }
